f9749cfbf032e856e0da12d911f49e84b25f784a
clementperrotaimvp <clementperrot@aimvp.fr>
2024-11-19 09:00:37 +0100

    modif

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def analyze_existing_logs():
     
     # Extract IP and occurrence using zeek-cut and save to extract.csv
     try:
-        subprocess.run(f"zeek-cut id.resp_p < {log_file} | sort | uniq -c | sort -rn | head -n 10 > {extract_file}", shell=True, check=True)
+        subprocess.run(f"zeek-cut id.resp_p < {log_file} | sort | uniq -c | sort -rn | head -n 10 > {extract_file}",cwd=f"/home/{user}/log/", shell=True, check=True)
     except subprocess.CalledProcessError as e:
         raise HTTPException(
             status_code=500, detail=f"Failed to extract data: {e.stderr}"
