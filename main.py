b7c541c029e0150277c82386379357febbb95176
clementperrotaimvp <clementperrot@aimvp.fr>
2024-11-26 04:11:39 +0100

    modif

--- main.py
+++ main.py
@@ -143,3 +143,71 @@ def analyze_existing_logs():
     return JSONResponse(
         content={"status": "success", "logs": extract_content}
     )
+
+
+
+
+@app.get("/analyze2")  
+def analyze2_existing_logs():
+    """
+    Endpoint to analyze a log file produced by Zeek.
+    
+    """
+    user = "p4"
+
+    # Create directories if they don't exist
+    pcap_dir = Path(f"/home/{user}/pcap_file")
+    log_dir = Path(f"/home/{user}/log")
+    zeek_dir = Path(f"/home/{user}/zeek_script")
+    
+    pcap_dir.mkdir(parents=True, exist_ok=True)
+    log_dir.mkdir(parents=True, exist_ok=True)
+    zeek_dir.mkdir(parents=True, exist_ok=True)
+
+    #cleaning old files
+    tcpdump_file = Path(f"/home/{user}/pcap_file/tcptraffic.pcap")
+    if tcpdump_file.exists():
+        tcpdump_file.unlink()
+    log_file = Path(f"/home/{user}/log/conn.log")
+    if log_file.exists():
+        log_file.unlink()
+    extract_file = Path(f"/home/{user}/pcap_file/extract.csv")
+    if extract_file.exists():
+        extract_file.unlink()
+    
+    
+    log_file = f"/home/{user}/log/conn.log"
+    extract_file = f"/home/{user}/log/extract.csv"
+
+
+    # Execute a Zeek analysing command
+    #zeek -i enp7s0 detect_icmp_dos_attack.zeek
+    try:
+        result = subprocess.run(["/usr/local/zeek/bin/zeek","-i", "lo", f"/home/{user}/zeek_script/detect_icmp_dos_attack.zeek"],cwd=f"/home/{user}/log/", capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Zeek command failed: {e.stderr}"
+        )
+    
+    # Check if the log file exists
+    if not Path(log_file).exists():
+        raise HTTPException(
+            status_code=404, detail=f"The file log file {log_file} does not exist."
+        )
+    
+    # Extract IP and occurrence using zeek-cut and save to extract.csv
+    # extract_top_ports("p4", "conn.log", "extract.csv")
+    
+    # Read the content of the log file
+    try:
+        with open(extract_file, "r") as f:
+            extract_content = f.readlines()
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error reading the extract file: {str(e)}"
+        )
+
+    # Return the lines of the log file
+    return JSONResponse(
+        content={"status": "success", "logs": extract_content}
+    )
