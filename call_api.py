48ccd70b3a4f41bd8534ade2e5d43d8e839b8c26
clementperrotaimvp <clementperrot@aimvp.fr>
2024-11-28 06:59:13 +0100

    o

--- call_api.py
+++ call_api.py
@@ -18,38 +18,46 @@ def call_api(url):
         print("Failed with status code:", response.status_code)
 
 if __name__ == "__main__":
-    t1 = datetime.now()
-    process = subprocess.Popen(["ping", "-f", "10.0.0.2"])
-    url1 = "http://10.0.0.2:8000/analyze2"
-    json_message = call_api(url1)
-    process.terminate()  
-    process.wait()  
-    if json_message and "time" in json_message:
-        t2_str = json_message["time"]
-        t2 = datetime.strptime(t2_str,"%Y-%m-%d %H:%M:%S.%f")
-    if json_message and "IP" in json_message:
-        IP = json_message["IP"]
-    detection_time = (t2 - t1).total_seconds() 
-    if IP!="NULL":
-        print(f"Temps de detection attack : {detection_time} for the IP address: {IP}")
-    else: 
-        print(f"Temps de detection flow normal  : {detection_time} ")
+    time_attack = []
+    time_ok = []
+    for i in range(3):
+        t1 = datetime.now()
+        process = subprocess.Popen(["ping", "-f", "10.0.0.2"])
+        url1 = "http://10.0.0.2:8000/analyze2"
+        json_message = call_api(url1)
+        process.terminate()  
+        process.wait()  
+        if json_message and "time" in json_message:
+            t2_str = json_message["time"]
+            t2 = datetime.strptime(t2_str,"%Y-%m-%d %H:%M:%S.%f")
+        if json_message and "IP" in json_message:
+            IP = json_message["IP"]
+        detection_time = (t2 - t1).total_seconds() 
+        if IP!="NULL":
+            print(f"Temps de detection attack : {detection_time} for the IP address: {IP}")
+        else: 
+            print(f"Temps de detection flow normal  : {detection_time} ")
+        time_attack.append(detection_time)
 
-    t1 = datetime.now()
-    process = subprocess.Popen(["ping", "10.0.0.2"])
-    url1 = "http://10.0.0.2:8000/analyze2"
-    json_message = call_api(url1)
-    process.terminate()  
-    process.wait()  
-    if json_message and "time" in json_message:
-        t2_str = json_message["time"]
-        t2 = datetime.strptime(t2_str,"%Y-%m-%d %H:%M:%S.%f")
-    if json_message and "IP" in json_message:
-        IP = json_message["IP"]
-    detection_time = (t2 - t1).total_seconds() 
-    if IP!="NULL":
-        print(f"Temps de detection attack : {detection_time} for the IP address: {IP}")
-    else: 
-        print(f"Temps de detection flow normal  : {detection_time} ")
+        t1 = datetime.now()
+        process = subprocess.Popen(["ping", "10.0.0.2"])
+        url1 = "http://10.0.0.2:8000/analyze2"
+        json_message = call_api(url1)
+        process.terminate()  
+        process.wait()  
+        if json_message and "time" in json_message:
+            t2_str = json_message["time"]
+            t2 = datetime.strptime(t2_str,"%Y-%m-%d %H:%M:%S.%f")
+        if json_message and "IP" in json_message:
+            IP = json_message["IP"]
+        detection_time = (t2 - t1).total_seconds() 
+        if IP!="NULL":
+            print(f"Temps de detection attack : {detection_time} for the IP address: {IP}")
+        else: 
+            print(f"Temps de detection flow normal  : {detection_time} ")
+        time_ok.append(detection_time)
+    print("Moyenne detection attack  : ", sum(time_attack)/3)
+    print("Moyenne detection OK  : ", sum(time_ok)/3)
+
 
   
